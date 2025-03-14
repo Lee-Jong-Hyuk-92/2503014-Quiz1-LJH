299fad41bcfb2e0b190b812f6c8e351206ac200c
Jonghyuk Lee <sptzkvp@gmail.com>
2025-03-14 02:53:10 +0000

    25.03.14 퀴즈

--- q5.py
+++ q5.py
@@ -5,25 +5,25 @@ b = pd.Series([68.5, 60.3, 53.4, 74.1, 80.7], name='weight')
 c = pd.Series([180, 165, 155, 178, 185], name ='height')
 
 ## 1. human DataFrame 만들기
-human = pd.DataFrame(None)
+human = pd.DataFrame({'age': a, 'weight': b, 'height': c})
 print(human,'\n')
 
 def main():
     ### 2. loc(), iloc() 함수를 이용하여 `age`를 추출
-    print(human.loc[None],'\n')
-    print(human.iloc[None],'\n')
+    print(human.loc[:,'age'],'\n')
+    print(human.iloc[:,0],'\n')
     
     ### 3. loc(), iloc() 함수를 이용하여 `weight`와 `height`만 추출
-    print(human.loc[None],'\n')
-    print(human.iloc[None],'\n')
+    print(human.loc[:,'weight':],'\n')
+    print(human.iloc[:,1:],'\n')
      
     sex = ['F','M','F','M','F']
     ### 4.새로운 데이터 `sex`를 `human`에 추가
-    human.loc[None] = None
+    human.loc[:,'sex'] = sex
     print(human,'\n')
     
     ### 5. `human`에서 `height`를 삭제
-    tmp = None
+    tmp = human.loc[:, human.columns != 'height']
     print(tmp,'\n')
 
 
